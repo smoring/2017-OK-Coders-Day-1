57745d8d06b7ae72768c8b456d756981131b7216
Susan Moring <susanmoring@susans-air.hsc.net.ou.edu>
2017-03-25 13:05:25 -0500

    Made server in index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// this is a comment in js
+var http = require('http');
+
+const PORT=8080;
+
+function handleRequest (req,resp){
+	resp.end('<h1>Welcome to my webpage</h1>');
+}
+
+var server = http.createServer(handleRequest);
+
+//start our server
+server.listen(PORT, function(){
+	//This is a callback, get used to it now
+	console.log("Server is running on port: %s", PORT);
+});
